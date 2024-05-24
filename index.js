e75994696fd760ce1c89e403a892fbcab1e500b6
Pavel Asafov <pavelaof@gmail.com>
2024-05-24 14:25:23 +0300

    add layout to whole app

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cards = document.querySelectorAll('.card')
 const discussProjectBtn = document.querySelector('.greeting_btn')
 const mainPage = document.getElementById('main_page')
 const formPage = document.querySelector('.form_page_outer_cont')
+const successPage = document.querySelector('.success_cont')
+const formPageMain = document.querySelector('.form_page_main')
+const form = document.forms[0]
+const closePageBtn = document.querySelector('.close_btn')
 
 showCards(0)
 
@@ -19,8 +23,21 @@ function openFormPage() {
   formPage.style.display = 'block'
 }
 
+function submitForm(event) {
+  event.preventDefault()
+  
+  formPageMain.style.display = 'none'
+  successPage.classList.add('success_cont_show')
+}
+
+function closePage() {
+  location.reload()
+}
+
 dots.forEach((dot, index) => {
   dot.addEventListener('click', () => showCards(index))
 })
 
 discussProjectBtn.addEventListener('click', openFormPage)
+form.addEventListener('submit', submitForm)
+closePageBtn.addEventListener('click', closePage)
