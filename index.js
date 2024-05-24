3b1197fc3ebb9b9c0738c5fcfb45f46c4c06a37e
Pavel Asafov <pavelaof@gmail.com>
2024-05-24 11:41:09 +0300

    form page layout

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const dots = document.querySelectorAll('.dot')
 const cards = document.querySelectorAll('.card')
+const discussProjectBtn = document.querySelector('.greeting_btn')
+const mainPage = document.getElementById('main_page')
+const formPage = document.querySelector('.form_page_outer_cont')
 
 showCards(0)
+
+function showCards(index) {
+  cards.forEach(card => card.style.display = 'none')
+  dots.forEach(dot => dot.classList.remove('active'))
+  
+  cards[index].style.display = 'flex'
+  dots[index].classList.add('active')
+}
+
+function openFormPage() {
+  mainPage.style.display = 'none'
+  formPage.style.display = 'block'
+}
 
 dots.forEach((dot, index) => {
   dot.addEventListener('click', () => showCards(index))
 })
 
-function showCards(index) {
-
-  cards.forEach(card => card.style.display = 'none')
-  dots.forEach(dot => dot.classList.remove('active'))
-
-  cards[index].style.display = 'flex'
-  dots[index].classList.add('active')
-}
+discussProjectBtn.addEventListener('click', openFormPage)
